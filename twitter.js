57d10833e9450a6151679118eae7649a7b9eac9e
rishigoel <goelrishi12@gmail.com>
2016-01-01 01:12:26 -0500

    Cleaned up js code

--- twitter.js
+++ twitter.js
@@ -1,25 +1,13 @@
-function kLowry () {
-
-var text = "Kyle Lowry"; 
-window.open("https://twitter.com/intent/tweet?button_hashtag=NBAVote "+text);
-
-
-	
-}
-
-function derozan () {
-
-var text = "Demar Derozan"; 
-window.open("https://twitter.com/intent/tweet?button_hashtag=NBAVote "+text);
-
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('kLow').addEventListener('click', kLowry);
+  document.getElementById('kLow').addEventListener('click', function() {
+  	window.open("https://twitter.com/intent/tweet?button_hashtag=NBAVote Kyle Lowry");
+  });
 
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('demar').addEventListener('click', derozan);
+  document.getElementById('demar').addEventListener('click', function() {
+  	window.open("https://twitter.com/intent/tweet?button_hashtag=NBAVote Demar Derozan");
+  });
 
 });
